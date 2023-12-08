d547968073a596f88a7880eef41bfb5cd7d368ee
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-12-08 10:34:21 +0300

    enabled Hystrix and HystrixDashboard dependencies

--- src/main/java/com/techg/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/com/techg/cloudgateway/CloudGatewayApplication.java
@@ -2,9 +2,12 @@ package com.techg.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
